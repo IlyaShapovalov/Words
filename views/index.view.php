258142880dcaca48b0289910f7db086d1ac4b682
Ilya Shapovalov <104982033+IlyaShapovalov@users.noreply.github.com>
2022-05-20 18:05:46 +0200

    Update search bar styling

--- views/index.view.php
+++ views/index.view.php
@@ -1,10 +1,9 @@
-<div>
-    <br>
-    <form  action="" method="GET" >
-    <input type="text" name="search" id="search">
-    <input type="submit" value="Search" >
+<div class="search-container flex ">
+    <form class="flex bg-light search-form" action="" method="GET" >
+        <input class="search-bar bg-light" type="text" name="search" id="search">
+        <input type="hidden" value="Search" >
+        <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1.7em" height="1.7em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 16 16"><path fill="#f68888" d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0a5.5 5.5 0 0 1 11 0z"/></svg>
     </form>
-    <br>
 </div>
 <div>
     <?php if (isset($_GET['search'])): ?>
