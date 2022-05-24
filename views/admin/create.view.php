7447297fbe5d4c6d78eee7dbfc4f833a11f4fd6c
Ilya Shapovalov <104982033+IlyaShapovalov@users.noreply.github.com>
2022-05-24 14:00:35 +0200

    Update styling of create and edit pages

--- views/admin/create.view.php
+++ views/admin/create.view.php
@@ -1,7 +1,7 @@
 <div class="flex crud-wraper">
-    <h4 class="ff-fancy fs-600 fc-main crud-title" >Add new term:</h4>
-    <form class="crud-form crud-form-an" action="" method="post">
-        <input type="text" class="crud-form-input crud-form-term bg-light fs-500 letter-spacing-3 " placeholder="New term..." name="term" id="term" ></input>
+    <h2 class="ff-fancy fs-600 fc-main crud-title" >Add new term:</h2>
+    <form class="crud-form crud-form-an" autocomplete="off" action="" method="post">
+        <input type="text" class="crud-form-input crud-form-term bg-light fs-500 letter-spacing-3 " placeholder="New term..." name="term" id="term"/>
         <textarea class="crud-form-input crud-form-def bg-light fs-500 letter-spacing-3" placeholder="Definition..." name="description" id="description" cols="20" rows="5"></textarea>
         <input class="crud-form-submit fs-600" type="submit" name="create" value="CREATE" >
     </form>
--- views/admin/edit.view.php
+++ views/admin/edit.view.php
@@ -1,17 +1,9 @@
-<div>  
-    <h4>Edit term</h4>
-    <form action="" method="post">
+<div class="flex crud-wraper">  
+    <h2 class="ff-fancy fs-600 fc-main crud-title">Edit term:</h2>
+    <form class="crud-form crud-form-an" action="" method="post">
         <input type="hidden" value="<?= $_GET['key'] ?>" name="original-term" />
-        <div class="form-group">
-            <label for="term" id="term">Edit term:</label>
-            <input type="text" class="form-control" name="edited-term" id="edited-term" value="<?= $view_bag['term']; ?>" cols="20" rows="5"></textarea>
-        </div>
-        <div class="form-group">
-            <label for="edited-description" id="edited-description">Edit description:</label>
-            <textarea class="form-control" name="edited-description" id="edited-description" cols="20" rows="5"><?= $view_bag['def']; ?></textarea>
-        </div>
-        <div class="form-group">
-            <input type="submit" name="edit" value="Edit" >
-        </div>
+        <input type="text" class="crud-form-input crud-form-term bg-light fs-500 letter-spacing-3 " name="edited-term" id="edited-term" value="<?= $view_bag['term']; ?>"/>
+        <textarea class="crud-form-input crud-form-def bg-light fs-500 letter-spacing-3" name="edited-description" id="edited-description" cols="20" rows="5"><?= $view_bag['def']; ?></textarea>
+        <input class="crud-form-submit fs-600" type="submit" name="edit" value="EDIT" >
     </form>
 </div>
