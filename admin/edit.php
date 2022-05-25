c9182f49f69f7b9745c411a6ed288b4099843a0c
Ilya Shapovalov <104982033+IlyaShapovalov@users.noreply.github.com>
2022-05-25 14:35:35 +0200

    Update edit and delete features

--- admin/edit.php
+++ admin/edit.php
@@ -6,4 +6,8 @@ require_once ( '../app/app.php');
 
 Data::edit_term($_GET['key']);
 
+if (isset($_POST['cancel'])) {
+    redirect('admin.php');
+}
+
 view(APP_ROOT . '/views/admin/edit');
--- views/admin/delete.view.php
+++ views/admin/delete.view.php
@@ -1,8 +1,8 @@
 <div class="flex crud-wraper">
     <h2 class="ff-fancy fs-700 fc-main crud-message">Do you really think <span><?= $_GET['delete'] ?></span> is not graceful enough?</h2>
-    <form  class="delete-submit" method="post">
+    <form  class="submit-group" method="post">
         <input type="hidden" value="<?= $_GET['delete'] ?>" name="deleted-term" />    
-        <input class="delete-submit-button fs-600" type="submit" name="cancel" value="CANCEL">
-        <input class="delete-submit-button fs-600" type="submit" name="delete" value="DELETE" >
+        <input class="submit-button fs-600" type="submit" name="cancel" value="CANCEL">
+        <input class="submit-button fs-600" type="submit" name="delete" value="DELETE" >
     </form>
 </div>
